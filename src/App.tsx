df57c4c47487369deef06ac677e06a3e4ea4a1f4
pablo-pc <pablo-362@hotmail.com>
2022-05-24 18:04:02 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import logo from "./logo.svg";
+import "./App.css";
+import {useAppSelector, useAppDispatch} from "./hooks/hooks";
+import {incremented, amountAdded} from "./redux/features/counterSlice";
+
+function App() {
+  const value = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img alt="logo" className="App-logo" src={logo} />
+        <p>Hello Vite + React!</p>
+        <button type="button" onClick={() => dispatch(incremented())}>
+          Increment 1
+        </button>
+        <button type="button" onClick={() => dispatch(amountAdded(3))}>
+          Increment 3
+        </button>
+
+        <p>{value}</p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
